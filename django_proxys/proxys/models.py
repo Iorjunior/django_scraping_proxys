a426476b3b5a47e1d820455c8ca1319a1f207bcf
iorjunior <juniorcoda@gmail.com>
2022-02-18 19:00:36 -0300

    Create Proxy model

--- django_proxys/proxys/models.py
+++ django_proxys/proxys/models.py
@@ -1,3 +1,15 @@
+from urllib import response
 from django.db import models
 
-# Create your models here.
+
+class Proxy(models.Model):
+    ip_address = models.CharField(max_length=15)
+    port = models.CharField(max_length=5)
+    protocol = models.CharField(max_length=5)
+    anonymity = models.CharField(default='None', max_length=4)
+    country = models.CharField(max_length=56)
+    region = models.CharField(max_length=56)
+    city = models.CharField(max_length=56)
+    uptime = models.DecimalField(decimal_places=2, max_digits=4)
+    response = models.IntegerField()
+    transfer = models.IntegerField()
